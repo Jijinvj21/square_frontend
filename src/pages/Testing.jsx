5ee5797392c5fe32b809315c60632a2bf485f3f3
Jijinvj21 <jijinvjvazhippilly@gmail.com>
2025-07-16 08:36:27 +0530

    feat: Update Testing page to use gatsby-plugin-react-helmet for improved Open Graph and Twitter metadata

--- src/pages/Testing.jsx
+++ src/pages/Testing.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import img from "../../public/og-image.png"
 
 function Testing() {
   const [title, setTitle] = useState('Dynamic OG Image');
   const [description, setDescription] = useState('Generated with React & Canvas');
   const [backgroundImage, setBackgroundImage] = useState('');
-
   // Fetch static background image
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/photos/1')
@@ -27,8 +27,8 @@ function Testing() {
           property="og:description"
           content="Explore the projects and experience of Jijin VJ, a passionate frontend developer skilled in building modern UI/UX with React and Framer Motion."
         />
-        <meta property="og:image" content="/og-image.png" />
-        <meta property="og:url" content="https://square-frontend-ten.vercel.app/" />
+        <meta property="og:image" content="https://jijin-vj.vercel.app/og-image.png" />
+        <meta property="og:url" content="https://jijin-vj.vercel.app/" />
         <meta property="og:site_name" content="Jijin Portfolio" />
         <meta property="og:type" content="website" />
         <meta property="og:image:width" content="1200" />
@@ -41,7 +41,7 @@ function Testing() {
           name="twitter:description"
           content="Explore the portfolio of Jijin VJ, React developer focused on performance, animation, and great UI."
         />
-        <meta name="twitter:image" content="/og-image.png" />
+        <meta name="twitter:image" content="https://jijin-vj.vercel.app/og-image.png" />
       </Helmet>
 
       <h1>Static OG Image Generator</h1>
@@ -69,7 +69,7 @@ function Testing() {
       {backgroundImage && (
         <div className="preview">
           <h2>OG Image Preview:</h2>
-          <img src={backgroundImage} alt="OG Image" style={{ maxWidth: '100%' }} />
+          <img src={img} alt="OG Image" style={{ maxWidth: '100%' }} />
         </div>
       )}
     </div>
